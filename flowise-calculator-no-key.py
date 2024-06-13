ae7428f8f4d40280943926565005b351888cd7bf
Liam Stone <stonediggity@gmail.com>
2024-06-13 14:29:10 +1000

    Update flowise-calculator-no-key.py

--- flowise-calculator-no-key.py
+++ flowise-calculator-no-key.py
@@ -1,35 +1,49 @@
-import os
-from schemas import OpenAIChatMessage
+from typing import List, Union, Generator, Iterator
 from pydantic import BaseModel
+import os
 import requests
-
 
 class Pipeline:
     class Valves(BaseModel):
+        # No API key needed for Flowise API
         pass
 
     def __init__(self):
+        # Optionally, you can set the id and name of the pipeline.
+        # Best practice is to not specify the id so that it can be automatically inferred from the filename, so that users can install multiple versions of the same pipeline.
+        # The identifier must be unique across all pipelines.
+        # The identifier must be an alphanumeric string that can include underscores or hyphens. It cannot contain spaces, special characters, slashes, or backslashes.
+        # self.id = "openai_pipeline"
         self.name = "Flowise Pipeline"
         self.valves = self.Valves()
+        pass
 
     async def on_startup(self):
+        # This function is called when the server is started.
         print(f"on_startup:{__name__}")
         pass
 
     async def on_shutdown(self):
+        # This function is called when the server is stopped.
         print(f"on_shutdown:{__name__}")
         pass
 
     def pipe(
         self, user_message: str, model_id: str, messages: List[dict], body: dict
     ) -> Union[str, Generator, Iterator]:
+        # This is where you can add your custom pipelines like RAG.
         print(f"pipe:{__name__}")
+
+        print(messages)
+        print(user_message)
 
         API_URL = "https://flowiseai-railway-production-092d.up.railway.app/api/v1/prediction/70e8ec5f-1d51-42ea-968c-6be37b5275de"
 
-        headers = {}
+        headers = {
+            "Content-Type": "application/json"
+        }
 
-        payload = {**body, "question": user_message}
+        payload = {**body}
 
         if "user" in payload:
             del payload["user"]
@@ -38,8 +52,21 @@ class Pipeline:
         if "title" in payload:
             del payload["title"]
 
+        print(payload)
+
         try:
-            response = requests.post(API_URL, json=payload, headers=headers)
-            return response.json()
+            r = requests.post(
+                url=API_URL,
+                json=payload,
+                headers=headers,
+                stream=True,
+            )
+
+            r.raise_for_status()
+
+            if body.get("stream"):
+                return r.iter_lines()
+            else:
+                return r.json()
         except Exception as e:
             return f"Error: {e}"
